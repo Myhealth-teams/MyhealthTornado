3eceadf0a819924eefcf378a936eaff8ca2f5f7b
ZJDTop <154469562@qq.com>
2019-11-27 14:56:09 +0800

    聊天室

---
+++ Chat/test.py
@@ -0,0 +1,36 @@
+import os
+
+from tornado import httpserver, ioloop
+from tornado.web import RequestHandler, url, Application
+from tornado.options import define,options
+
+
+
+
+
+define("port", default=5000, type=int)
+class IndexHandler(RequestHandler):
+    def get(self):
+        self.render("index.html")
+class IndexHandler2(RequestHandler):
+    def get(self):
+        self.render("index1.html")
+
+
+settings = {
+        "static_path" : os.path.join(os.getcwd(), "static"),    #ico文件路径
+        "template_path" : os.path.join("templates"), #视图文件路径
+        "cookie_secret" : "DONT_SHOW_SECRET",
+        "xsrf_cookies" :True,  #预防跨站攻击
+         "debug":True
+}
+if __name__ == '__main__':
+    options.parse_command_line()
+    app =Application([
+            url(r"/index/",IndexHandler),
+            url(r'/index1/', IndexHandler2)
+        ],**settings)
+
+    http_server = httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    ioloop.IOLoop.current().start()
---
+++ chat.py
@@ -0,0 +1,77 @@
+import datetime
+import os
+import tornado.web
+import tornado.ioloop
+import tornado.options
+import tornado.httpserver
+
+from tornado.options import define,options
+from tornado.websocket import WebSocketHandler
+# from tornado.ioloop import IOLoop
+
+define("port", default=8000, type=int)
+
+
+
+
+class ChatHandler(WebSocketHandler):
+    users = set() # 用户ID
+    client_id = 1
+    def open(self):
+
+         # 每个用户独有的ID
+        ChatHandler.client_id +=1
+        self.client_id = ChatHandler.client_id
+        self.users.add(self)
+        for u in self.users:  #登录告知所有人
+                data = {
+                    "user":self.client_id,
+                    "time":datetime.datetime.now().strftime("%H:%M:%S"),
+                    "state":"1"
+                }
+                u.write_message(data)
+        print(self.client_id,"连接成功")
+
+    def on_message(self, message):
+        print(self.client_id,"正在发消息")
+        for u in self.users: # 向在在线用户广播消息
+            data = {
+                "user": self.client_id,
+                "time": datetime.datetime.now().strftime("%H:%M:%S"),
+                "state":"发消息",
+                "msg":message
+            }
+            u.write_message(data)
+
+    def on_close(self):
+        print(self.client_id,"退出聊天室")
+        self.users.remove(self)
+        for u in self.users:
+            data = {
+                "user": self.client_id,
+                "time": datetime.datetime.now().strftime("%H:%M:%S"),
+                "state": "0",
+            }
+            u.write_message(data)
+
+    def check_origin(self, origin):
+        return True
+
+settings = {
+        "static_path" : os.path.join(os.getcwd(), "static"),    #ico文件路径
+        "template_path" : os.path.join("templates"), #视图文件路径
+        "cookie_secret" : "DONT_SHOW_SECRET",
+        "xsrf_cookies" :True,  #预防跨站攻击
+         "debug":True
+}
+
+
+if __name__ == '__main__':
+    tornado.options.parse_command_line()
+    app = tornado.web.Application([
+            (r"/chat/", ChatHandler),
+        ],**settings)
+
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
