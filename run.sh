421e1020ea00c3b8212a7bc07974efc754c95d31
xuhui <pighui233@163.com>
2019-11-28 14:40:38 +0800

    更新run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 echo 'starting MyhealthTornado project'
 cd /usr/src/MyhealthTornado
-gunicorn -w 1 -b 0.0.0.0:8000 -k tornado chat:application -D
+gunicorn -w 1 -b 0.0.0.0:8000 -k tornado chat:application
